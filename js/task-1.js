8293068d490f6c518e92e76648bfe8d32c05dc1b
Adams360 <44724909+Adams360@users.noreply.github.com>
2021-10-19 01:30:54 +0300

    finish assignment

--- js/task-1.js
+++ js/task-1.js
@@ -3,22 +3,68 @@
 const data = fetch('./task-1.json')
     .then(response => response.json())
     .then(data => {
-        const placeholder = document.querySelector('#placeholder'); 
-        const pets = data.pets;
+        const placeholder = document.querySelector('#placeholder');
 
-        const story = 
-             `${data.name.first} ${data.name.last} 
-             lives in ${data.address.city} City, 
-             ${data.name.first} is ${data.status} to ${data.partner.name} and they both
-             have ${data.kids.number} kids ${data.kids.boys.numbers} boys and a girl, 
-             ${data.kids.number}, Will and Michelle.
-             John is 8, Will is 4 and Michelle is 2 years old.
-             Steve works at MTV as a marketing director, he has been working there for
-             the past 6 years.
-             The fishers have ${pets.length} pets: ${pets.join(', ')}.` 
+        const a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+        const b = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+        function inWords(num) {
+            if ((num = num.toString()).length > 9) return '???';
+            n = ('000000000' + num).substr(-9).match(/^(\d{2})(\d{2})(\d{2})(\d{1})(\d{2})$/);
+            if (!n) return; var str = '';
+            str += (n[5] != 0) ? ((str != '') ? 'and ' : '') + (a[Number(n[5])] || b[n[5][0]] + ' ' + a[n[5][1]]) : '';
+            return str;
+        }
+
+        let boysNum = 0,
+            girlsNum = 0;
+
+        data.children.forEach(child => {
+            if (child.gender === 'male') {
+                boysNum += 1;
+            }
+
+            if (child.gender === 'female') {
+                girlsNum += 1;
+            }
+        });
+
+
+        const printNumber = (number) => number > 1 ? number : 'a'
+        const printPlural = (word, number) => word + (number > 1 ? 's' : '')
+        
+        const sentence = `${printNumber(boysNum)} ${printPlural('boy', boysNum)} and ${printNumber(girlsNum)} ${printPlural('girl', girlsNum)}`
+
+        const story =
+            `<strong>${data.name.first} ${data.name.last}</strong>
+             <hr>
+             lives in <strong>${data.address.city}</strong> City, 
+             <hr>
+             <strong>${data.name.first}</strong> is <strong>${data.status}</strong>
+             to <strong>${data.partner.name}</strong>
+             <hr>
+             and they both have <strong>${inWords(data.children.length)}</strong> kids
+             <hr>
+             <strong>${sentence}</strong>
+             <hr>
+             <strong>${data.children[0].name}, ${data.children[1].name}</strong> 
+             and <strong>${data.children[2].name}</strong>.
+             <hr>
+             <strong>${data.children[0].name}</strong> is <strong>${data.children[0].age}</strong>, 
+             ${data.children[1].name} is <strong>${data.children[1].age}</strong> 
+             and <strong>${data.children[2].name}</strong> is 
+             <strong>${data.children[2].age}</strong> years old.
+             <hr>
+             <strong>${data.name.first}</strong> works at <strong>${data.job.location.toUpperCase()}</strong> as a <strong>${data.job.position}</strong>,
+             <hr>
+             he has been working there for the past 
+             <strong>${data.job.periodInMonths / 12}</strong> years.
+             <hr>
+             The fishers have <strong>${data.pets.length}</strong> 
+             pets: <strong>${data.pets.join(', ')}</strong>.`
 
         placeholder.innerHTML = story;
-        
+
     }).catch((error) => {
         console.error('Error:', error);
     });
--- js/task-3.js
+++ js/task-3.js
@@ -2,28 +2,55 @@ $(function () {
 
     $('#app button').on('click', function () {
 
-        $.ajax({
-            method: 'GET',
-            url: 'http://ip-api.com/json/?fields=continent,country,regionName,city,query',
-            dataType: 'json',
-            contentType: "application/json; charset=utf-8",
 
-            success: function (data) {
+        var state = $('#placeholder').attr('data-state');
+        var heading = $('.placeholder__content h1').text('data-state');
 
-                const cityItem     = document.querySelector('#city span'),
-                      countryItem  = document.querySelector('#country span');
-                      regionItem   = document.querySelector('#region span'),
-                      continet     = document.querySelector('#continent span'),
+        console.log(state);
 
-                
+        if (state === 'start') {
 
-                cityItem.innerHTML = data.city;
-                countryItem.innerHTML = data.country;
-                regionItem.innerHTML = data.regionName;
-                continet.innerHTML = data.city;
+            $('.placeholder__content h1').text('Your location is found!');
+            $('.placeholder__content p').text('Take a look or reset.');
+            $('.placeholder__content button').text('Reset');
 
-            }
-        })
+            $('#placeholder').attr('data-state', 'results');
+
+
+            $.ajax({
+                method: 'GET',
+                url: 'http://ip-api.com/json/?fields=continent,country,regionName,city,query',
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+
+
+                success: function (data) {
+
+                    const cityItem = document.querySelector('#city span'),
+                          countryItem = document.querySelector('#country span');
+                          regionItem = document.querySelector('#region span'),
+                          continet = document.querySelector('#continent span'),
+
+
+                    cityItem.innerHTML    = data.city;
+                    countyItem.innerHTML = data.country;
+                    regionItem.innerHTML = data.regionName;
+                    continet.innerHTML   = data.continent;
+
+                }
+            })
+
+
+        } else if (state === 'results') {
+
+            $('.placeholder__content h1').text('Want to find out where you are?');
+            $('.placeholder__content p').text('Click on the button below to see details on your location');
+            $('.placeholder__content button').text('Find Location');
+
+            $('#placeholder').attr('data-state', 'start');
+
+        }
+
 
     });
 
